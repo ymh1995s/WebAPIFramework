550153cdf91447170a9351f5bcbcc19ecacb8a56
DESKTOP-SLNADE2\user <qq11123@naver.com>
2026-04-21 02:41:01 +0900

    [feat] 기본 프론트앤드구성
    - 내 생각인데 비동기 적용이 안된 듯

--- Framework/Framework.Admin/Program.cs
+++ Framework/Framework.Admin/Program.cs
@@ -6,6 +6,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Services.AddHttpClient("ApiClient", client =>
+{
+    client.BaseAddress = new Uri(builder.Configuration["ApiBaseUrl"] ?? "http://localhost:5058");
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
